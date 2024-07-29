53ab0e416f94f8a33712777aaf9fea779a84567d
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-07-29 15:45:33 +0500

    added 5-work js

---
+++ 5-JS/main.js
@@ -0,0 +1,79 @@
+//Control Flow 2-turga bo'lindi
+
+// 1.Loops
+// 2.Conditioal statements
+
+//Loop- Biron bir dasturdagi kodini cycle through(aylanib chiqish), qayta-qayta ishga tushirish uchun
+
+
+
+
+// // For
+
+// for(let numb=null; numb < 6;numb ++){
+//     console.log(1);
+// }
+
+
+// let users = ["Azizbek" , "Alibek" ,  "Jahon","Firdavs", "Guli", "Xojik"]
+
+// // for (let user = 0; user < users.length; user++) {
+// //     console.log(users[user]);
+// // }
+
+
+// if(users[3]==="Firdavs" || users[0] === "Jahon" 
+    
+// ) {
+//     console.log("Hello");
+// }
+
+//  else  {
+//     console.log("Don't not");
+// }
+
+
+
+// Switch case
+// let colors = ["red" , "yellow", "green"];
+// let color=colors[0]
+// switch(color) {
+//     case "red":
+//     console.log("yurma");
+//     break
+//     case "yellow": 
+//     console.log("tayyorlan");
+//     break;
+//     case "green":
+//     console.log("olg'a");
+//     break;
+//     default:
+//     console.log("boshqa rang qoy");
+// }
+
+// // While
+// let colors = ["red" , "yellow", "green"];
+// let i = 0;
+// while(i < colors.length) {
+//     console.log(colors[i]);
+//     i++
+// }
+
+
+
+// for (let i = 0 ; i<colors.length; i++) {
+//     console.log(colors[i]);
+// }
+
+
+
+//DoWhile
+
+let colors = ["red" , "yellow", "green"];
+let i = 0;
+do{
+console.log(colors[i]);
+    i= i+2
+}
+while(i < colors.length) {
+}
