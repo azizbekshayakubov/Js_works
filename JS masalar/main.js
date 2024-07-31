b5768d37fa46cdfc46f268ea20e4eb66f2b04a71
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-07-31 17:19:23 +0500

    added masala js

---
+++ JS masalar/main.js
@@ -0,0 +1,59 @@
+//Masala-1
+// function findAverga(array) {
+    
+//     let x = 0
+// if (array.length > 0) {
+//     for (let index = 0; index < array.length; index++) {
+
+//     x= (array[index]+x )
+// }
+// return x/array.length   
+// } else {
+//     return 0
+// }
+
+// }
+
+// let array = [1,2,3,4,5,6]
+
+// console.log(findAverga(array));
+
+
+
+//Masala-2
+
+// function sum(numbers) {
+    
+//     let x = 0
+// if (numbers.length > 0) {
+//     for (let index = 0; index < numbers.length; index++) {
+
+//     x= (numbers[index]+x )
+// }
+// return x   
+// } else {
+//     return 0
+// }
+
+// }
+
+// let array = [1,2,3,4,5]
+
+// console.log(sum(array));
+
+
+//Masala-3
+
+function ageYour(name) {
+
+    if (name[0] ==="R" || name [0]=== "r") {
+        return `${name} play banjo`
+    } else{
+        return `${name} does not play banjo`
+    }
+}
+
+ageYour(name)
+
+console.log(ageYour("Rahim"));
+
