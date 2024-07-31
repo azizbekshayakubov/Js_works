eaf65b8b2fc8771d3a49ee307102cb92d7199f02
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-07-31 16:35:14 +0500

    added 6-works

---
+++ 6-JS/main.js
@@ -0,0 +1,49 @@
+
+// function sum (x,y ) {
+//   console.log(x+y);
+   
+// }
+// sum(2, 5)
+
+// function fullname (name , surname) {
+// console.log(`${name} ${surname}`);
+// }
+
+// fullname("Azizbek" , "Shoyoqubov")
+
+
+// let arr = [1,2,3,4,5]
+// let arr2 = [1,2,3,4,5,6,7]
+
+// function reveseArr (array) {
+
+//     for (let i = array.length -1; i>0; i--) {
+//         console.log(array[i]);
+//     }
+// }
+
+// reveseArr(arr2)
+
+// function greeting (name) {
+//     console.log("Hello " + name  + ":)");
+// }
+
+// let nama = prompt("Enter a name")
+
+// greeting(nama)
+
+
+function fullname (name , surname) {
+    
+    let fullname = name + surname
+    console.log("Hello");
+
+    return fullname
+
+
+}
+
+
+
+console.log( fullname("Azizbek", " Beks"));
+
