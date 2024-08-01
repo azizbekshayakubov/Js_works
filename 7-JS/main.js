3223d75391f44274d91e003cffa86c2b3ffd0045
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-08-01 12:48:19 +0500

    numbers

--- 7-JS/main.js
+++ 7-JS/main.js
@@ -1,21 +1,21 @@
-let users =  {
-   id:1,
-   name:"Firdavs",
-   surname:"Solijonov",
-   age:18,
-   isMarried: false,
-   phone: ["+9989111122333","+9989334555544","+998900009988"],
-   loginIN: function(name) {
-     return (`${this.name} Login in, and his age is ${this.age}` );
-   },
+// let users =  {
+//    id:1,
+//    name:"Firdavs",
+//    surname:"Solijonov",
+//    age:18,
+//    isMarried: false,
+//    phone: ["+9989111122333","+9989334555544","+998900009988"],
+//    loginIN: function(name) {
+//      return (`${this.name} Login in, and his age is ${this.age}` );
+//    },
 
-   logOut () {
-    return `${this.name} is log out `
-   },
-   phoneMeto(){
-    return this
-   }
-}
+//    logOut () {
+//     return `${this.name} is log out `
+//    },
+//    phoneMeto(){
+//     return this
+//    }
+// }
 
 //Contextgacha bo'lgan yo'lni korsatadigan keyword- this
 
@@ -25,14 +25,14 @@ let users =  {
 // console.log(this.location);
 
 
-let obj = {
-    title:"lorem",
-    whattile(){
-        return this
-    }
-}
+// let obj = {
+//     title:"lorem",
+//     whattile(){
+//         return this
+//     }
+// }
 
-console.log(obj.whattile());
+// console.log(obj.whattile());
 
 
 
@@ -55,3 +55,46 @@ console.log(obj.whattile());
 // users = null
 
 // console.log(users ,person);
+
+let radius = 10;
+const pi =3.14;
+
+// console.log(radius,pi);
+
+// math operators +,-,*,/,**,
+
+// let result = 10%3
+
+// console.log(result);
+
+// console.log(10 / 2);
+
+// let result = pi * radius ** 2
+
+//order of operation
+
+// let result = 5 * (10-3) ** 2
+
+let like = 10
+
+// like = like + 1
+// like++
+// like--
+// like = like+10
+// like += 12
+// like -= 8
+// like /= 2
+
+// like *= 2
+
+// //Nan -not a number
+
+// console.log(5/"salom");
+
+// console.log(like);
+
+
+let result = 'the blog has ' + like + ' likes'
+
+console.log(result);
+
