bc7fa28ee517d9ccc6df961c40628029609c47bf
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-08-21 17:33:33 +0500

    ddess

--- 8-JS/main.js
+++ 8-JS/main.js
@@ -1,5 +1,8 @@
- const title = document.querySelectorAll(".title")   
- const title2 = document.querySelectorAll("#title2")   
+//  const title = document.querySelectorAll(".title")   
+//  const title2 = document.getElementById("title")   
+//  const title = document.getElementsByClassName("title")   
+ const title = document.getElementsByTagName("h1")   
+
 
  console.log(title);
- 
+  
