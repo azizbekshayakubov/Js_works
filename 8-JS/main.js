7c1b4d37d732688e7f5fc458c8f12d2c7274019d
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-08-22 14:23:52 +0500

    addeerr

--- 8-JS/main.js
+++ 8-JS/main.js
@@ -3,11 +3,21 @@
 //  const title = document.getElementsByClassName("title")   
 //  const title = document.getElementsByTagName("h1") 
 
-const title = document.querySelector("p")
+// const subtitle = document.querySelector("p")
+// const title = document.querySelectorAll("h1")
+
+// // subtitle.innerHTML = "<h1>Hello worddddd</h1> "
+// // title.innerText = "salom dunyo"
+
+// for (let index = 0; index < title.length; index++) {
+    
+// console.log(title[index]);
+// title[index].innerText = "Salom"
+// }
+
+const link = document.querySelector("a");
+
+console.log(link.getAttribute("href"));
 
 
-title.innerHTML = "<h1>Hello worddddd</h1> "
-// title.innerText = "salom dunyo"
-
-console.log(title);
   
