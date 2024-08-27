308d42e5108e185b81876b05a6df265b05b63d57
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-08-27 10:33:50 +0500

    added 9 js

---
+++ 9-JS/main.js
@@ -0,0 +1,23 @@
+
+const arr = [23,54,65]
+
+// for (let index = 0; index < arr.length; index++) {
+// //    console.log(arr[index]);
+   
+// }
+
+
+//syntx sugar
+
+// let name =  "Azizbek Shoyoqubov"
+
+let obj = {
+    name: "Azizbek",
+    suname:"Shoyoqubov",
+    age: 25
+}
+
+for (el of obj) {
+    console.log(el);
+    
+}
