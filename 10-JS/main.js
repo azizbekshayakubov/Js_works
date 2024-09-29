6a2afd957552193b791058dd581414256614c2de
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-09-29 23:03:38 +0500

    filter

--- 10-JS/main.js
+++ 10-JS/main.js
@@ -4,21 +4,21 @@ let users = [
         id:1,
         surname: "Shoyoqubov",
         name: "Azizbek",
-        isStudent: "true",
+        isStudent: true,
         budjet:1250
     },
     {
         id:2,
         surname: "Shermuhamedov",
-        name: "Mushtariy",
-        isStudent: "false",
+        name: "Odil",
+        isStudent: false,
         budjet:1100
     },
     {
         id:3,
         surname: "To'laganov",
         name: "Odil",
-        isStudent: "false",
+        isStudent: false,
         budjet:1350
     },
 ]
@@ -28,8 +28,22 @@ const arr = [123,456,6554]
 
 // let user = arr.includes(123)
 
-let user = users.find(function(item,index, arr){
-    return item.name == "Odil"
+// let user = users.find(function(item,index, arr){
+//     return item.name == "Odil"
+// })
+
+
+let user = users.filter(function(item,index, arr){
+    // return item.isStudent == false
+   
+//    let bud = item.budjet > 1000
+
+    if (item.budjet > 1900) {
+     
+        return item
+    } 
+
+   
 })
 
 console.log(user);
