e265554deb392e6228e6d23f68313d4b13695904
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-10-12 20:00:53 +0500

    added some

--- 10-JS/main.js
+++ 10-JS/main.js
@@ -33,17 +33,43 @@ const arr = [123,456,6554]
 // })
 
 
-let user = users.filter(function(item,index, arr){
-    // return item.isStudent == false
+// let user = users.filter(function(item,index, arr){
+//     // return item.isStudent == false
    
-//    let bud = item.budjet > 1000
+// //    let bud = item.budjet > 1000
 
-    if (item.budjet > 1900) {
+//     if (item.budjet > 1900) {
      
-        return item
-    } 
+//         return item
+//     } 
 
    
+// })
+
+// console.log(user);
+
+// const ages = [40,50,19]
+
+// let checkAge = ages.every(function(ages){
+//     return ages > 18
+// })
+
+// console.log(checkAge);
+
+// const ages = [40,15,19]
+
+// let checkAge = ages.some(function(ages){
+//     return ages > 18
+// })
+
+// console.log(checkAge);
+
+const ages = [40,50,19,14]
+
+let checkAge = ages.sort(function(ages){
+    return ages > 18
 })
 
-console.log(user);
+console.log(checkAge);
+
+
