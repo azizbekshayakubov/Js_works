1613bbe8ac76f3a8cea0ce131fe085d11d459a0a
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-10-16 09:46:36 +0500

    addedddd

--- 11-JS/main.js
+++ 11-JS/main.js
@@ -3,11 +3,18 @@
 let  a = "stengin"
 let b = a 
 b = "sister"
-console.log(a);
+// console.log(a);
 
 
 let obj = {
     id:1,
     name: "Anvar",
-    
+    type: "Strong"
 }
+
+
+let x = obj
+ 
+x=obj
+
+console.log(obj);
