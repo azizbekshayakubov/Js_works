1c7d9d2c806f87f9a8cd3d7f2e0260847183360f
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-10-21 11:28:13 +0500

    added

---
+++ 12-JS/main.js
@@ -0,0 +1,16 @@
+const buttons = document.querySelectorAll ("button")
+
+const freeSpace = document.querySelector ("#freeSpace")
+
+let arrBtn = Array.from(buttons)
+
+arrBtn.map(item=> {
+
+    item.addEventListener('click' , (e) =>{
+         let valu = e.target.textContent
+
+         switch(valu) {
+                
+         
+    })
+})
