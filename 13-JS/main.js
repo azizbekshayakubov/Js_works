68c8a134be22780af60099a8f911a3893818e38f
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-10-23 17:29:53 +0500

    added % quiz

--- 13-JS/main.js
+++ 13-JS/main.js
@@ -1,8 +1,11 @@
 const form = document.querySelector(".quiz-form")
+const result = document.querySelector(".result")
+const userScore = document.querySelector(".score")
 
 const correktAnwor = ["B","A","A","B"]
 
 form.addEventListener('submit', e =>{
+    let score = 0
     e.preventDefault()
     let userAnwor = [
         e.target.q1.value, 
@@ -10,5 +13,15 @@ form.addEventListener('submit', e =>{
         e.target.q3.value,
         e.target.q4.value
     ];
-    
+
+    for (let index = 0; index < userAnwor.length; index++) {
+        if (userAnwor[index] ===  correktAnwor[index]) {
+           score += 25
+        }
+        
+    }
+    scrollTo(0,0)
+    userScore.textContent = `${score}%`
+    result.classList.remove("d-none")
+
 })
