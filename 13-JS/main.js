e42604973070f272099343dae0a9b522038e2ac8
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-10-24 21:59:50 +0500

    added copy

--- 13-JS/main.js
+++ 13-JS/main.js
@@ -1,17 +1,36 @@
 const itmes = document.querySelectorAll("li")
 const list = document.querySelector("ul")
 const btn = document.querySelector("button")
+const div = document.querySelector("div")
 
-itmes.forEach((item)=>{
- 
-   item.addEventListener("click" , (e) =>{
-    e.target.classList.toggle("active")
-     
-   })
+div.addEventListener("mouseover" , (e)=>{
+    console.log(`x:${e.offsetX}, y:${e.offsetY}`);
+    
 })
+
+// itmes.forEach((item)=>{
+ 
+//    item.addEventListener("click" , (e) =>{
+//     // e.target.classList.toggle("active")
+//     console.log("Clicked");
+//     e.stopPropagation()
+     
+//    })
+// })
 
 
 btn.addEventListener("click" , (e)=>{
     list.innerHTML += `<li>New items</li>`
     
 })
+
+
+list.addEventListener("click" , (e)=>{
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("active")
+    }
+    e.stopPropagation()
+    
+})
+
+
