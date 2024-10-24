ee22ed8a654d2ad0af87a64c96ab38a31b172965
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-10-24 10:04:52 +0500

    added

--- 13-JS/main.js
+++ 13-JS/main.js
@@ -1,44 +1,17 @@
-const form = document.querySelector(".quiz-form")
-const result = document.querySelector(".result")
-const userScore = document.querySelector(".score")
+const itmes = document.querySelectorAll("li")
+const list = document.querySelector("ul")
+const btn = document.querySelector("button")
 
-const correktAnwor = ["B","A","A","B"]
-
-form.addEventListener('submit', e =>{
-    let score = 0
-    e.preventDefault()
-    let userAnwor = [
-        e.target.q1.value, 
-        e.target.q2.value,
-        e.target.q3.value,
-        e.target.q4.value
-    ];
-
-    for (let index = 0; index < userAnwor.length; index++) {
-        if (userAnwor[index] ===  correktAnwor[index]) {
-           score += 25
-        }
-        
-    }
-    scrollTo(0,0)
-    result.classList.remove("d-none")
-    let i =0
-    let timer = setInterval(() => {
-    userScore.textContent = `${i}%`
-    
-    if (i == score) {
-        clearInterval(timer)
-    }else{
-        i++
-    }
-    
-}, 25);
-
-    
-
+itmes.forEach((item)=>{
+ 
+   item.addEventListener("click" , (e) =>{
+    e.target.classList.toggle("active")
+     
+   })
 })
 
 
-// setTimeout(() => {
+btn.addEventListener("click" , (e)=>{
+    list.innerHTML += `<li>New items</li>`
     
-// }, 1000);
+})
