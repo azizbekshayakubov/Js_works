9c8696971461023de71d7b10d84ceb7984a53b7f
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-11-25 22:00:20 +0500

    readyStatr

--- 14-JS/main.js
+++ 14-JS/main.js
@@ -1,7 +1,13 @@
 const request =  new  XMLHttpRequest()
+console.log(request.readyState);
+
 
 //GET, POST,DELETA,UPDATE
 
+request.addEventListener('readystatechange' , ()=>{
+    console.log(request, request.readyState);
+    
+})
 
 request.open('GET' , 'https://jsonplaceholder.typicode.com/todos')
 request.send()
