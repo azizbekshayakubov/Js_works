3707de95192f94888af1aa93b4fe4f31f5182c8a
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-11-26 09:26:25 +0500

    fetch

--- 14-JS/main.js
+++ 14-JS/main.js
@@ -1,15 +1,34 @@
+const getTodos = (callback) =>{
 const request =  new  XMLHttpRequest()
-console.log(request.readyState);
-
-
-//GET, POST,DELETA,UPDATE
-
 request.addEventListener('readystatechange' , ()=>{
-    console.log(request, request.readyState);
-    
+    if (request.readyState === 4 && request.status === 200) {
+       let data = JSON.parse(request.responseText)
+       callback(undefined,data)
+    } else if(request.readyState === 4){
+        callback("cloud not fetch" , undefined) 
+    }
 })
 
+
+// CRUD 
 request.open('GET' , 'https://jsonplaceholder.typicode.com/todos')
 request.send()
+}
+const container = document.querySelector('div')
+getTodos((err, data)=>{
 
-
+    if (!data) {
+        console.log(err);
+        container.innerHTML = "<h1>cloud not fetch</h1>"
+        
+    } else {
+        data.map(item =>{
+        let list = document.createElement('h3')
+        let span = document.createElement('span')
+        list.textContent = `Title:${item.title}`
+        span.textContent = `Completed:${item.completed}`
+        container.append(list)
+        container.append(span)
+       })
+    }
+})
