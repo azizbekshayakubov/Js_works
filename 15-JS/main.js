d0b5ac004e5f83cdd03b3ce595a2c1a1dd0070f6
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-11-26 21:33:20 +0500

    added json fail

---
+++ 15-JS/main.js
@@ -0,0 +1,27 @@
+const getTodos = (url ,callback) =>{
+const request =  new  XMLHttpRequest()
+request.addEventListener('readystatechange' , ()=>{
+    if (request.readyState === 4 && request.status === 200) {
+       let data = JSON.parse(request.responseText)
+       callback(undefined,data)
+    } else if(request.readyState === 4){
+        callback("cloud not fetch" , undefined) 
+    }
+})
+
+
+// CRUD 
+request.open('GET' , url)
+request.send()
+}
+const container = document.querySelector('div')
+getTodos("users.json" , (err, data)=>{
+
+    if (!data) {
+        console.log(err);
+        
+    } else {
+        console.log(data);
+       }
+    }
+)
