bbab65c17fb85675d468e60da47c1f4d327e7ac6
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-11-27 21:50:58 +0500

    then.json

--- 15-JS/main.js
+++ 15-JS/main.js
@@ -1,18 +1,93 @@
-const getTodos = (url ,callback) =>{
-const request =  new  XMLHttpRequest()
-request.addEventListener('readystatechange' , ()=>{
-    if (request.readyState === 4 && request.status === 200) {
-       let data = JSON.parse(request.responseText)
-       callback(undefined,data)
-    } else if(request.readyState === 4){
-        callback("cloud not fetch" , undefined) 
-    }
-})
-// CRUD 
-request.open('GET' , url)
-request.send()
+function fetchUr() {
+  let request  = fetch('https://jsonplaceholder.typicode.com/todos/')
+      request.then(response => response.json())
+      .then(json => 
+       json.map((item => {
+       console.log(item);
+       
+        if (item.id === 10) {
+            item.password = "234"
+        }
+
+       const container = document.querySelector ('div')
+       const heading = document.createElement('h1')
+       const password = document.createElement('h3')
+       heading.textContent = item.title 
+       password.textContent = item.completed
+       container.append(heading)
+       container.append(password)
+       
+      }
+      )))
 }
 
+fetchUr()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const getTodos = (url ,callback) =>{
+// const request =  new  XMLHttpRequest()
+// request.addEventListener('readystatechange' , ()=>{
+//     if (request.readyState === 4 && request.status === 200) {
+//        let data = JSON.parse(request.responseText)
+//        callback(undefined,data)
+//     } else if(request.readyState === 4){
+//         callback("cloud not fetch" , undefined) 
+//     }
+// })
+// // CRUD 
+// request.open('GET' , url)
+// request.send()
+// }
 
 
 // const container = document.querySelector('div')
