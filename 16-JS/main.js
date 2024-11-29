1c57b662c8f7a89138a1d9afa9f5b9e3993ce431
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-11-29 10:58:42 +0500

    1-hours

--- 16-JS/main.js
+++ 16-JS/main.js
@@ -2,7 +2,28 @@ const now = new Date ()
 
 console.log(now);
 
-console.log("Year:");
+console.log("Year: ", now.getFullYear());
+console.log("Month", now.getMonth() +1);
+
+//Hafta kuni
+
+console.log("Day", now.getDay());
+
+// Sana 
+console.log("Day:",now.getDate());
+
+//Hours
+console.log("Hours", now.getHours());
+console.log("Minuts", now.getMinutes());
+console.log("Sekunds", now.getSeconds());
+
+
+//Timestap
+console.log(now.getTime());
+console.log(now.toDateString());
+console.log(now.toLocaleString());
+console.log(now.toTimeString());
+
 
 
 
