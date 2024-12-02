cfa0cae419d7a8adc52d7ffb3ed12912669563c6
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-12-02 16:12:32 +0500

    50-minuts

--- 17-JS/main.js
+++ 17-JS/main.js
@@ -8,17 +8,39 @@
 
 // console.log(malibo,matiz);
 
-class Cars {
-    model;
+// class Cars {
+//     model;
+//     color;
+// }
+
+// let malibo = new Cars()
+
+
+// const Malibu = {
+//     name: "Malibu",
+//     color:"Red",
+//     cost:30000,
+//     type:"Supercar"
+// }
+
+
+class Car {
+    name;
     color;
+    cost;
+    type;
+
+    constructor(name,color,cost,type) {
+        this.name=name
+        this.color=color
+        this.cost=cost
+        this.type=type
+    }
 }
 
-let malibo = new Cars()
+const Malibu = new Car("Malibu", "red", 30000, "sedan")
+const Spark = new Car("Spark", "black", 50000, "supercar")
+console.log(Malibu);
+console.log(Spark);
 
 
-const Malibu = {
-    name: "Malibu",
-    color:"Red",
-    cost:30000,
-    type:"Supercar"
-}
