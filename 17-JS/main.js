a36c8c21aa6d98758b831642571255ec0199327c
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-12-02 13:28:00 +0500

    OPP

---
+++ 17-JS/main.js
@@ -0,0 +1,24 @@
+//  function Cars (model , color) {
+//     this.model= model
+//     this.color= color  
+//  }
+
+// let malibo = new  Cars("Model" ,"Red")
+// let matiz = new  Cars("Sedan" ,"Black")
+
+// console.log(malibo,matiz);
+
+class Cars {
+    model;
+    color;
+}
+
+let malibo = new Cars()
+
+
+const Malibu = {
+    name: "Malibu",
+    color:"Red",
+    cost:30000,
+    type:"Supercar"
+}
