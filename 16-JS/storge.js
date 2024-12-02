7421d86d4f4611a267e4d134a063cb7e8c5dc047
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-12-02 10:25:49 +0500

    clear

--- 16-JS/storge.js
+++ 16-JS/storge.js
@@ -1,27 +1,53 @@
-//store data in localstorge
+// //store data in localstorge
 
-localStorage.setItem("username", "Shoyoqubov")
-localStorage.setItem("age", "25")
-localStorage.setItem("isMerrid", true)
+// localStorage.setItem("username", "Shoyoqubov")
+// localStorage.setItem("age", "25")
+// localStorage.setItem("isMerrid", true)
 
-//get data
-let a =localStorage.getItem("username") 
-let b =localStorage.getItem("age") 
-let d =localStorage.getItem("isMerrid") 
-console.log(a)
-console.log(b)
-console.log(d)
+// //get data
+// let a =localStorage.getItem("username") 
+// let b =localStorage.getItem("age") 
+// let d =localStorage.getItem("isMerrid") 
+// console.log(a)
+// console.log(b)
+// console.log(d)
 
-//update
+// //update
 
-localStorage.setItem("isMerrid" ,false)
-localStorage.username = "BEk"
-localStorage.username = "BEk"
+// localStorage.setItem("isMerrid" ,false)
+// localStorage.username = "BEk"
+// localStorage.username = "BEk"
+// localStorage.clear()
+
+// //delete
+
+// // localStorage.removeItem("age")
 
 
-//delete
+const todos = [
+    { 
+      id:1,
+      title:"lorem"
+    },
+     { 
+      id:2,
+      title:"lorem44"
+    },
+     { 
+      id:3,
+      title:"loremrere"
+    }, { 
+      id:4,
+      title:"loremfasfs"
+    },
+];
 
-// localStorage.removeItem("age")
+let jstodos = JSON.stringify(todos)
+
+localStorage.setItem("td", jstodos)
+let b = localStorage.getItem("td")
+
+console.log(JSON.parse(b));
 
 
 
