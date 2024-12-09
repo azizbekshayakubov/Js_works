72c309dbcce6bcd7bf6c93a80f66d60f95613b4b
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-12-09 13:14:14 +0500

    15-minuts

--- 18-JS/main.js
+++ 18-JS/main.js
@@ -1,46 +1,82 @@
 
-const candy  ={
-  name: "Chocotella",
-  weight: "500g",
-  color:"black",
-  ingredients:[ 'sugar' , 'cacao' , 'nut' , 'milk']
-}
+// const candy  ={
+
+//   name: "Chocotella",
+//   weight: "500g",
+//   color:"black",
+//   ingredients:[ 'sugar' , 'cacao' , 'nut' , 'milk']
+// }
 
 
-class Candy {
-  //members
-  //behaviours/method
-  //field/attribete/key
+// class Candy {
+//   //members
+//   //behaviours/method
+//   //field/attribete/key
 
-  name
-  weight
-  color
-  ingredients  
+//   name
+//   weight
+//   color
+//   ingredients  
    
-  constructor(n, w, c, i) {
-    this.name = n
-    this.weight = w
-    this.color = c 
-    this.ingredients = i
-  }
+//   constructor(n, w, c, i) {
+//     this.name = n
+//     this.weight = w
+//     this.color = c 
+//     this.ingredients = i
+//   }
 
-  isOpen(){
-    return `${this.name} is Open`;
+//   isOpen(){
+//     return `${this.name} is Open`;
     
-  }
+//   }
 
-}
+//  //set / get
+
+//  get inform () {
+//   return  ` its ${this.name}  and is has ${this.weight}`
+//  }
+   
+// }
 
 
-const snikers = new Candy (
+// const snikers = new Candy (
   
 
-  "Snikers" ,"250g", "black" , [ 'sugar' , 'cacao' , 'nut' , 'milk']
-)
+//   "Snikers" ,"250g", "black" , [ 'sugar' , 'cacao' , 'nut' , 'milk']
+// )
 
-const Chocotella = new Candy (
-  "Chocotella" ,"350g", "red" , [ 'sugddar' , 'cacado' , 'nudt' , 'mildk']
-)
+// const Chocotella = new Candy (
+//   "Chocotella" ,"350g", "red" , [ 'sugddar' , 'cacado' , 'nudt' , 'mildk']
+// )
 
-console.log(snikers.isOpen());
-console.log(Chocotella.isOpen());
+// console.log(snikers.isOpen());
+// console.log(Chocotella.inform);
+
+
+class Tools {
+  constructor (book, pencil,lunchBox, era){
+    this.book = book
+    this.pencil = pencil
+    this.container = lunchBox
+    this.eraser = era
+  }
+
+  //ecncapulation
+
+  get bag() {
+    return `Bu bagni ichida ${this.book}, ${this.pencil} , ${this.container}, ${this.eraser}  bor`
+  }
+
+  set changeBox (arr) {
+    return typeof arr [0] === "string" ? this.book : undefined
+  }
+}
+
+
+
+//instance
+
+const obj = new Tools ( "Hello Book" , "Ruchka" ,"Box", "Eraa" )
+Tools.changeBox = ["chimyo"]
+console.log(obj.bag);
+console.log();
