0ed2d6a2029a84ca304d7109f52e7fb54029cdcb
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-12-10 16:59:47 +0500

    math

---
+++ 19-JS/main.js
@@ -0,0 +1,10 @@
+let number = 6.4
+
+// console.log(Math.random());
+// console.log(Math.floor(number));
+// console.log(Math.ceil(number));
+console.log(Math.trunc(number));
+console.log(Math.round(number));
+console.log(Math.round(number));
+console.log(Math.pow(4, 1/2));
+console.log(Math.abs(-5));
