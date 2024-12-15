3aabbce102868d06c93754d377129b4f09b5fcf5
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-12-15 11:54:35 +0500

    added options

--- 19-JS/main.js
+++ 19-JS/main.js
@@ -9,32 +9,71 @@
 // console.log(Math.pow(4, 1/2));
 // console.log(Math.abs(-5));
 
-// Super class
-class Animal {
-    constructor(name){
-    this.name = name
-    }
+// // Super class
+// class Animal {
+//     constructor(name){
+//     this.name = name
+//     }
 
-    speak (){
-      return `My name is ${this.name}`
-    }
-}
-
-//drived class
-class Dog extends Animal {}
-
-const rext = new Dog ("Rex")
-
-console.log(rext.speak());
+//     speak (){
+//       return `My name is ${this.name}`
+//     }
+// }
 
 //drived class
-class Cat extends Animal {
-    
-    constructor (rang){
-      super()
-    this.color = rang
+
+
+class Animal{
+  constructor(options) {
+    this.name = options.ism;
+    this.hasTail = options.dumiBormi;
+    this.color = options.rangi
+    this.type = options.turi
+
+  }
+ }
+
+
+class Dog extends Animal {
+  constructor(options) {
+    super(options)
+    this.paroda = options.nasl
   }
 }
 
-const Garfild = new Cat("Garfild", "Red")
-console.log(Garfild);
+const rext = new Dog ({
+  ism: "Rex",
+  dumiBormi: true,
+  rangi: "red",
+  turi: "Yertqich",
+  nasl: "Bo'ribosar"
+})
+
+
+console.log(rext);
+
+
+class Cat extends Animal {
+  constructor(options) {
+    super(options) 
+    this.aga = options.yosh
+  }
+}
+
+
+const Mosh = new Cat ({
+   ism: "Mosh",
+  dumiBormi: true,
+  rangi: "wihit",
+  turi: "uy xayvoni",
+  yosh: 24
+})
+
+
+console.log(Mosh);
+
+
+
+
+
+
