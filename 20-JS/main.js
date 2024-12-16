04a6e22b93c2cfec52496a373756823c444b19d0
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2024-12-16 18:09:59 +0500

    25-minuts

---
+++ 20-JS/main.js
@@ -0,0 +1,32 @@
+// console.log(number);
+
+
+
+//  let name = "Qurbon"
+//  var number = 25
+//  let obj =  {
+//     id:1,
+//     username: "Cpl20"
+
+//  }
+
+// let a = 15 
+// {
+//     let a = 16
+// }
+
+// {
+//     let a;
+//     a=12
+// }
+
+// console.log(a);
+
+
+var a = 10;
+function sum () {
+    a =12 
+    return a
+}
+sum()
+console.log(a);
