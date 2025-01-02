69e097c9716efbeb72c324a4676659b66d76879b
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2025-01-02 12:04:52 +0500

    nulish operation

--- Finish-JS/main.js
+++ Finish-JS/main.js
@@ -5,4 +5,18 @@
  let arr = [...mev, ...meva]
 
  console.log(arr);
+
+ let  num = 50;
+
+ let  res = num > 0 ?  "to'g'ri" : "xato"
  
+//  console.log(res);
+ 
+
+ //nulish
+
+ let b  = undefined 
+
+let v = b ?? "undefi";
+
+console.log(v);
